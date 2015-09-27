b21ed2c24c7cdb79e938da7105f179c9ad6b6c63
Tuyen Chung <tuyen_chung@yahoo.com>
2015-09-27 13:45:39 -0700

    updated op login

--- client/opController.js
+++ client/opController.js
@@ -1,15 +1,11 @@
 LifeLine.controller('opController', function($scope, opFactory, $location){
 	$scope.login = function(){
-		if($scope.op.email == undefined || $scope.op.password == undefined){
-			$scope.err = "Invalid Credentials";
-		} else {
-			opFactory.login($scope.op, function(data){
-				if(data.email == $scope.op.email && data.password == $scope.op.password){
-					$location.path("/opDash");
-				} else {
-					$scope.err = "Invalid Credentials";
-				}
-			})
-		}
+		opFactory.login($scope.op, function(data){
+			if(data.email == $scope.op.email && data.password == $scope.op.password){
+				$location.path("/opDash");
+			} else {
+				$scope.err = "Invalid Credentials";
+			}
+		})
 	}
 })
--- client/static/factories/opFactory.js
+++ client/static/factories/opFactory.js
@@ -1,11 +1,6 @@
 LifeLine.factory('opFactory', function($http){
 	var factory = {};
 
-	// factory.getProducts = function(callback){
-	// 	$http.get('/all_products').success(function(output){
-	// 		callback(output);
-	// 	})
-	// }
 	factory.login = function(op, callback){
 		$http.post('/login', op).success(function(output){
 			callback(output);
